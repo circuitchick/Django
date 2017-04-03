577338c8a087b9bc445b9c0d34bf729879b56ec9
Kareena Hudson <kareena.k.hudson@aggiemail.usu.edu>
2017-04-03 01:18:23 +0000

    Hello django world views code

---
+++ Salutations/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SalutationsConfig(AppConfig):
+    name = 'Salutations'
---
+++ Salutations/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import time
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello, world. Greetings and Salutations!!!! The present date and time is... " + time.strftime("%c"))
